dd4f152e629c958e1801ce78a645e1641858ca77
agyvazar <tkachov.pavel@gmail.com>
2024-04-10 18:02:45 +0200

    26_remove_duplicates

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {3, 2, 2, 3};
-        int[] nums2 = {3, 2, 2, 3, 3};
-        int[] nums3 = {3};
-        int[] nums4 = {0,1,2,2,3,0,4,2};
-        int[] nums5 = {2,3,3};
-        int val = 3;
-        System.out.println(removeElement(nums5, val));
+        int[] nums1 = {1,1,2};
+        int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums2));
     }
 
-    public static int removeElement(int[] nums, int val) {
-        int occurrences = 0;
-        int left_idx = 0;
-        int right_idx = nums.length - 1;
-
-        if (right_idx == -1) return 0;
-
-        while (!indexesAreCloseEnough(left_idx, right_idx)) {
-            if (nums[left_idx] == val && nums[right_idx] == val) {
-                right_idx--;
-            } else if (nums[left_idx] == val && nums[right_idx] != val) {
-                nums[left_idx] = nums[right_idx];
-                nums[right_idx] = val;
-                right_idx--;
-                left_idx++;
-                occurrences++;
-            } else if (nums[left_idx] != val) {
-                left_idx++;
-                occurrences++;
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int currentNumber = nums[0];
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != currentNumber) {
+                currentNumber = nums[i];
+                nums[k] = nums[i];
+                k++;
             }
         }
-
-        if (right_idx == left_idx) {
-            if (nums[left_idx] != val) {
-                occurrences++;
-            }
-        } else {
-            if (nums[left_idx] == val && nums[right_idx] != val) {
-                nums[left_idx] = nums[right_idx];
-                nums[right_idx] = val;
-                occurrences++;
-            } else {
-                occurrences = nums[right_idx] != val ? occurrences + 2 : nums[left_idx] != val ? occurrences + 1 : occurrences;
-            }
-        }
-
-        return occurrences;
-    }
-
-    public static boolean indexesAreCloseEnough(int left, int right) {
-        int difference = right - left;
-        return difference == 0 || difference == 1;
+        return k;
     }
 }

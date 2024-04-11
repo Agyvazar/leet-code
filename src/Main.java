fdbc0bfa27899251980a5e633ff0c466257be935
agyvazar <tkachov.pavel@gmail.com>
2024-04-11 14:32:54 +0200

    189_rotate_array

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,37 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {2,2,1,1,1,2,2};
-        int[] nums2 = {0,0,1,1,1,1,2,2,3,3,4};
+        int[] nums = {1, 2};
+//        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
 
-        System.out.println(majorityElement(nums1));
+        rotate(nums, k);
     }
 
-    public static int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == candidate) {
-                count++;
-            } else {
-                count--;
-                if (count == 0) {
-                    candidate = nums[i];
-                    count++;
-                }
-            }
+    public static void rotate(int[] nums, int k) {
+        if (nums.length == 0 || nums.length == 1) return;
+        int j = 0;
+        int replacementsDone = 0;
+        while (replacementsDone < nums.length) {
+            replacementsDone = replacementsDone + replaceRound(nums, k, j);
+            j++;
         }
+        System.out.println(Arrays.toString(nums));
+    }
 
-        return candidate;
+    public static int replaceRound(int[] nums, int k, int initialIndex) {
+        int count = 1;
+        int i = initialIndex + k >= nums.length ? (initialIndex + k) % nums.length : initialIndex + k;
+        int swapLeftElement = nums[initialIndex];
+        int swapRightElement;
+        do {
+            swapRightElement = nums[i];
+            nums[i] = swapLeftElement;
+            count++;
+            i = i + k >= nums.length ? (i + k) % nums.length : i + k;
+            swapLeftElement = swapRightElement;
+        } while (i != initialIndex);
+        nums[i] = swapLeftElement;
+        return count;
     }
 }

303cd8bfbd8ce4c6fd776484024e949f394e6230
agyvazar <tkachov.pavel@gmail.com>
2024-04-11 10:50:22 +0200

    169_majority_element

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {1,1,2};
+        int[] nums1 = {2,2,1,1,1,2,2};
         int[] nums2 = {0,0,1,1,1,1,2,2,3,3,4};
-        int[] nums3 = {1,1,1,2,2,3};
-        int[] nums4 = {0,0,1,1,1,1,2,3,3};
-        int[] nums5 = {1,2,2,2};
-        int[] nums6 = {1,1};
-        int[] nums7 = {-3,-3,-2,-1,-1,0,0,0,0,0};
-        System.out.println(removeDuplicates(nums3));
+
+        System.out.println(majorityElement(nums1));
     }
 
-    public static int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return 1;
-        int k = 2;
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] != nums[k-1] || nums[i] != nums[k-2]) {
-                nums[k] = nums[i];
-                k++;
+    public static int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = nums[i];
+                    count++;
+                }
             }
         }
-        System.out.println(Arrays.toString(nums));
-        return k;
+
+        return candidate;
     }
 }

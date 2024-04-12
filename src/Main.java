15a317145fbf88d49919380303268762813fa9aa
agyvazar <tkachov.pavel@gmail.com>
2024-04-13 00:22:39 +0200

    121_best_buy_sell

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,43 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {5, 6, 4, 3, 10};
+        int[] nums1 = {7,1,5,3,6,4};
 
-        System.out.println(maxProfit(nums));
+        System.out.println(maxProfit(nums1));
     }
 
     public static int maxProfit(int[] prices) {
-        int length = prices.length;
         int profit = 0;
-        int min = prices[0];
+        int length = prices.length;
         int maxIdx = 0;
-        for (int start = 0; start < length - 1; start++) {
-            if (prices[start] > min) {
-                continue;
-            }
-            if (start < maxIdx) {
-                profit = profit + min - prices[start];
-                min = prices[start];
-                continue;
-            }
-            for (int end = start + 1; end < length; end++) {
-                if (prices[start] < prices[end]) {
-                    if (prices[end] - prices[start] > profit) {
-                        profit = prices[end] - prices[start];
-                        min = prices[start];
-                        maxIdx = end;
-                    }
-                }
-            }
+        int minIdx;
+        int start = 0;
+        while (start < length) {
+            maxIdx = findIdxOfMax(prices, start, length);
+            minIdx = findIdxOfMin(prices, start, maxIdx);
+            profit = Math.max(profit, prices[maxIdx] - prices[minIdx]);
+            start = maxIdx + 1;
         }
         return profit;
+    }
+
+    public static int findIdxOfMax(int[] prices, int start, int end) {
+        int maxIdx = start;
+        for (int i = start; i < end; i++) {
+            if (prices[i] >= prices[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+
+    public static int findIdxOfMin(int[] prices, int start, int end) {
+        int minIdx = 0;
+        for (int i = start; i < end; i++) {
+            if (prices[i] < prices[minIdx]) {
+                minIdx = i;
+            }
+        }
+        return minIdx;
     }
 }

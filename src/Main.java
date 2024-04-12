5e309a135186020f609b9d4cd3b8f29ca391fec4
agyvazar <tkachov.pavel@gmail.com>
2024-04-13 00:36:21 +0200

    121_best_buy_sell_optimized

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {5, 6, 4, 3, 10};
-        int[] nums1 = {7,1,5,3,6,4};
+        int[] nums = {5,6,4,3,10};
 
-        System.out.println(maxProfit(nums1));
+        System.out.println(maxProfit(nums));
     }
 
     public static int maxProfit(int[] prices) {
-        int profit = 0;
-        int length = prices.length;
-        int maxIdx = 0;
-        int minIdx;
-        int start = 0;
-        while (start < length) {
-            maxIdx = findIdxOfMax(prices, start, length);
-            minIdx = findIdxOfMin(prices, start, maxIdx);
-            profit = Math.max(profit, prices[maxIdx] - prices[minIdx]);
-            start = maxIdx + 1;
+        int buy = Integer.MAX_VALUE;
+        int sell = 0;
+        for (int price : prices) {
+            buy = Math.min(buy, price);
+            sell = Math.max(sell, price - buy);
         }
-        return profit;
-    }
-
-    public static int findIdxOfMax(int[] prices, int start, int end) {
-        int maxIdx = start;
-        for (int i = start; i < end; i++) {
-            if (prices[i] >= prices[maxIdx]) {
-                maxIdx = i;
-            }
-        }
-        return maxIdx;
-    }
-
-    public static int findIdxOfMin(int[] prices, int start, int end) {
-        int minIdx = 0;
-        for (int i = start; i < end; i++) {
-            if (prices[i] < prices[minIdx]) {
-                minIdx = i;
-            }
-        }
-        return minIdx;
+        return sell;
     }
 }

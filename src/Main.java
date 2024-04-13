c65a4029871730d9ab9ec21ba4cecf38d5ff4146
agyvazar <tkachov.pavel@gmail.com>
2024-04-13 11:23:06 +0200

    45_jump_game_ii

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3,2,1,0,4};
+        int[] nums = {1,2,1,1,1};
+        int[] nums1 = {10,9,8,7,6,5,4,3,2,1,1,0};
 
-        System.out.println(canJump(nums));
+        System.out.println(canJump(nums1));
     }
 
-    public static boolean canJump(int[] nums) {
-        if (nums.length == 1) return true;
-        int jumpRequired = 1;
-        for (int i = nums.length - 2; i > 0; i--) {
-            if (nums[i] < jumpRequired) {
-                jumpRequired++;
-            } else {
-                jumpRequired = 1;
+    public static int canJump(int[] nums) {
+        if (nums.length == 1) return 0;
+        int start = 0;
+        int jumps = 1;
+        while (start + nums[start] < nums.length - 1) {
+            start = findMaxIdx(nums, start + 1, nums[start]);
+            jumps++;
+        }
+        return jumps;
+    }
+
+    public static int findMaxIdx(int[] nums, int start, int jumpLength) {
+        int max = 0;
+        int maxIdx = start;
+        for (int i = start; i <= start + jumpLength - 1; i++) {
+            if (max <= nums[i] + i - maxIdx) {
+                maxIdx = i;
+                max = nums[i];
             }
         }
-        return jumpRequired <= nums[0];
+        return maxIdx;
     }
 }

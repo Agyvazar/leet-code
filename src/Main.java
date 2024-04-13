9683ff76b0154adcaacfed3fd8eb5f5e4a1678b0
agyvazar <tkachov.pavel@gmail.com>
2024-04-13 04:48:16 +0200

    55_jump_game

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {5,6,4,3,10};
+        int[] nums = {3,2,1,0,4};
 
-        System.out.println(maxProfit(nums));
+        System.out.println(canJump(nums));
     }
 
-    public static int maxProfit(int[] prices) {
-        if (prices.length == 0 || prices.length == 1) return 0;
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+    public static boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        int jumpRequired = 1;
+        for (int i = nums.length - 2; i > 0; i--) {
+            if (nums[i] < jumpRequired) {
+                jumpRequired++;
+            } else {
+                jumpRequired = 1;
             }
         }
-        return profit;
+        return jumpRequired <= nums[0];
     }
 }

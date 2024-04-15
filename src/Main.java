4b84d8cc3666b8bcd76ef9bf4abb6f2f869c0be1
agyvazar <tkachov.pavel@gmail.com>
2024-04-16 00:49:19 +0200

    380_insert_delete_getrandom

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3,0,6,1,5};
-
-        System.out.println(hIndex(nums));
-    }
-
-    public static int hIndex(int[] citations) {
-        int[] distribution = new int[1001];
-
-        for (int citation : citations) {
-            distribution[citation]++;
-        }
-
-        int accumulator = 0;
-
-        for (int i = distribution.length - 1; i > 0; i--) {
-            accumulator = accumulator + distribution[i];
-            if (accumulator >= i) return i;
-        }
-        return 0;
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1));
+        System.out.println(randomizedSet.remove(2));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.getRandom());
+        System.out.println(randomizedSet.remove(1));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.getRandom());
     }
 }

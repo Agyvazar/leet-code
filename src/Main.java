a7130693a6b20fd43743f3b6fb85af9a1ea6659e
agyvazar <tkachov.pavel@gmail.com>
2024-04-21 18:36:56 +0200

    58_length_of_last_word

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        String s = "MCMXCIV";
+        String s = "   fly me   to   the moon  ";
 
-        System.out.println(romanToInt(s));
+        System.out.println(lengthOfLastWord(s));
     }
 
-    public static int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
-
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-
-        int ans = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
-                ans -= m.get(s.charAt(i));
-            } else {
-                ans += m.get(s.charAt(i));
-            }
-        }
-
-        return ans;
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] array = s.split(" ");
+        return array[array.length - 1].length();
     }
 }

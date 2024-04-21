a6332e6e25a53e633f97c68ff86195cdf464a6a7
agyvazar <tkachov.pavel@gmail.com>
2024-04-21 16:02:35 +0200

    42_tapping_rain_water

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,34 @@
-import java.util.Stack;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ratings = {1,0,2};
-        int[] ratings1 = {1,2,2};
-        int[] ratings2 = {1,3,2,2,1};
-        int[] ratings3 = {1,2,87,87,87,2,1};
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-        System.out.println(candy(ratings3));
+        System.out.println(trap(height));
     }
 
-    public static int candy(int[] ratings) {
-        if (ratings.length == 0) return 0;
-        if (ratings.length == 1) return 1;
-        int result = 1;
-        int previous = 1;
-        int idxLocalMax = 0;
-        int localMaxCandy = 1;
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                previous = previous + 1;
-                result = result + previous;
-                idxLocalMax = i;
-                localMaxCandy = previous;
-            } else if (ratings[i] < ratings[i - 1]) {
-                if (previous == 1) {
-                    if (i - idxLocalMax < localMaxCandy) {
-                        result = result + i - idxLocalMax;
-                    } else {
-                        result = result + i - idxLocalMax + 1;
-                        localMaxCandy = localMaxCandy + 1;
-                    }
-                } else {
-                    previous = 1;
-                    result = result + previous;
-                }
-            } else if (ratings[i] == ratings[i - 1]) {
-                previous = 1;
-                idxLocalMax = i;
-                localMaxCandy = previous;
-                result = result + previous;
-            }
+    public static int trap(int[] height) {
+        if (height.length == 0 || height.length == 1) return 0;
+        int[] toRightPeaks = new int[height.length];
+        int[] toLeftPeaks = new int[height.length];
+
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            max = Math.max(height[i], max);
+            toRightPeaks[i] = max;
         }
-        return result;
+
+        max = 0;
+        for (int i = height.length - 1; i >= 0; i--) {
+            max = Math.max(height[i], max);
+            toLeftPeaks[i] = max;
+        }
+
+        int water = 0;
+
+        for (int i = 1; i < height.length - 1; i++) {
+            water = water + Math.max(Math.min(toRightPeaks[i - 1], toLeftPeaks[i + 1]) - height[i], 0);
+        }
+        return water;
     }
 }

a7171cd142f444b7a286edb2cfb0b6104a271d2a
agyvazar <tkachov.pavel@gmail.com>
2024-04-21 20:32:12 +0200

    14_longest_common_prefix

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "   fly me   to   the moon  ";
+        String[] strs = {"flower", "flow", "flight"};
 
-        System.out.println(lengthOfLastWord(s));
+        System.out.println(longestCommonPrefix(strs));
     }
 
-    public static int lengthOfLastWord(String s) {
-        s = s.trim();
-        String[] array = s.split(" ");
-        return array[array.length - 1].length();
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        int minLength = prefix.length();
+        for (int i = 1; i < strs.length; i++) {
+            minLength = Math.min(minLength, strs[i].length());
+            for (int j = 0; j < minLength; j++) {
+                if (prefix.charAt(j) != strs[i].charAt(j)) {
+                    minLength = j;
+                    break;
+                }
+            }
+        }
+        return prefix.substring(0, minLength);
     }
 }
